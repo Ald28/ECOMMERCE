432cae13abc9ef6858183fde211c9238ebf30365
jhersinAldana19 <jhersin.aldana@tecsup.edu.pe>
2024-06-13 12:21:21 -0500

    implementé el carrito de compras

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
+// src/App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
-import CategoryList from './components/CategoryList';
 import Header from './components/Header';
+import Cart from './components/Cart';
+import CategoryList from './components/CategoryList';
+import { CartProvider } from './context/CartContext';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/categories/')
+      .then(response => {
+        setCategories(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the categories!', error);
+      });
+  }, []);
 
   const handleLogin = () => {
     setIsAuthenticated(true);
@@ -22,32 +34,25 @@ const App = () => {
     setIsAuthenticated(false);
   };
 
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/categories/')
-      .then(response => {
-        setCategories(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching categories:', error);
-      });
-  }, []);
-
   return (
-    <Router>
-      <div className="App">
-        <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} categories={categories} />
-        <main className="container mt-4">
-          <Routes>
-            <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Home isAuthenticated={isAuthenticated} />} />
-            <Route path="/home" element={isAuthenticated ? <Home isAuthenticated={isAuthenticated} /> : <Navigate to="/" />} />
-            <Route path="/categories/:categoryId" element={<Home isAuthenticated={isAuthenticated} />} />
-            <Route path="/categories" element={<CategoryList />} />
-            <Route path="/register" element={<Register onLogin={handleLogin} />} />
-            <Route path="/login" element={<Login onLogin={handleLogin} />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Header isAuthenticated={isAuthenticated} handleLogout={handleLogout} categories={categories} />
+          <main className="container mt-4">
+            <Routes>
+              <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Home isAuthenticated={isAuthenticated} />} />
+              <Route path="/home" element={isAuthenticated ? <Home isAuthenticated={isAuthenticated} /> : <Navigate to="/" />} />
+              <Route path="/categories/:categoryId" element={<Home isAuthenticated={isAuthenticated} />} />
+              <Route path="/categories" element={<CategoryList />} />
+              <Route path="/register" element={<Register onLogin={handleLogin} />} />
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </CartProvider>
   );
 };
 
---
+++ frontend/src/context/CartContext.js
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingProductIndex = prevCart.findIndex(item => item.id === product.id);
+      if (existingProductIndex >= 0) {
+        // Actualiza la cantidad si el producto ya está en el carrito
+        const updatedCart = [...prevCart];
+        updatedCart[existingProductIndex].quantity += 1;
+        return updatedCart;
+      } else {
+        // Agrega el nuevo producto al carrito
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter(item => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
